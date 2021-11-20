0f9411d8d383b17444b6e2fcfe095ebdeb738449
moocstudent <343840681@qq.com>
2021-11-20 11:11:25 +0800

    Initial commit

---
+++ src/main/java/com/example/jsdemo/JsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsDemoApplication.class, args);
+    }
+
+}
